5935668db86e3f6d84b8955f5b93c185de558e86
Jesus <jflorespadilla@nevada.unr.edu>
2018-05-19 01:26:23 -0700

    Created Maze and added rotation script

--- UA1/Assets/Spin.cs
+++ UA1/Assets/Spin.cs
@@ -2,15 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class holaMundo : MonoBehaviour {
+public class Spin : MonoBehaviour {
+    public float speed = 3.0f;
 
-	// Use this for initialization
-	void Start () {
-        Debug.Log("Saludos, cabrones!");
-	}
-	
 	// Update is called once per frame
 	void Update () {
-		
+        transform.Rotate(0, speed, 0);		
 	}
 }
