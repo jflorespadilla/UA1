987469e762ee1c094b74050a871ed7f3fd34a20d
Jesus <jflorespadilla@nevada.unr.edu>
2018-06-14 20:03:19 -0700

    Added new enemy scripts

--- UA1/Assets/WanderingAI.cs
+++ UA1/Assets/WanderingAI.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WanderingAI : MonoBehaviour {
+    [SerializeField] private GameObject fireballPrefab;
+    private GameObject _fireball;
+
     private bool _alive;
 
     public float speed = 3.0f;
@@ -18,6 +21,12 @@ public class WanderingAI : MonoBehaviour {
             Ray ray = new Ray(transform.position, transform.forward);
             RaycastHit hit;
             if (Physics.SphereCast(ray, 0.75f, out hit)) {
+                GameObject hitObject = hit.transform.gameObject;
+                if (_fireball == null) {
+                    _fireball = Instantiate(fireballPrefab) as GameObject;
+                    _fireball.transform.position = transform.TransformPoint(Vector3.forward * 1.5f);
+                    _fireball.transform.rotation = transform.rotation;
+                }
                 if (hit.distance < obstacleRange) {
                     float angle = Random.Range(-110, 110);
                     transform.Rotate(0, angle, 0);
